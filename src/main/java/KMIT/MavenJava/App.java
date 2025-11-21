37684fe752e83f50d5cab3eb4382667101a2212c
zeeshan770212 <zeeshanar56@gmail.com>
2025-11-21 12:14:04 +0530

    Update App.java

--- src/main/java/KMIT/MavenJava/App.java
+++ src/main/java/KMIT/MavenJava/App.java
@@ -3,7 +3,7 @@ package KMIT.MavenJava;
 /**
  * Hello world!
  */
-public class App 
+public class App {
     public static void main(String[] args) {
         System.out.println("HELLO PEOPLE!");
     }
